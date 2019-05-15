b9360f91f3c2b814d6c3e183426ea4c67c175770
Кузнецов Максим <mkuznetsov@sinergo.ru>
2019-05-15 14:02:52 +0700

    fix code style

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-include('simple_html_dom.php');
+include 'simple_html_dom.php';
 
-$html = file_get_html('https://msk.kassir.ru/koncert/voleybolno-sportivnyiy-kompleks/lyube-odintsovo_2019-05-23');
-$data = [];
-$index = 0;
+(object)$html = file_get_html('https://msk.kassir.ru/koncert/voleybolno-sportivnyiy-kompleks/lyube-odintsovo_2019-05-23');
+(array)$data = [];
+(int)$index = 0;
 foreach ($html->find('#prices tbody') as $val) {
 	foreach ($val->children as $v) {
 		foreach ($v->children as $value) {
